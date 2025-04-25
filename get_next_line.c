9bbcebaf9a0cc691a1d95aa61dc1e12e5f02dfdd
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-25 20:05:37 +0200

    Mostly done, still some cases to handle

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:05:25 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/22 22:38:38 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/25 19:58:30 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 char	*get_next_line(int fd)
 {
-	
+    static char	*leftover = NULL;
+    char		*buffer;
+    int			line_len;
+    int			byte_read;
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || !(buffer = malloc(BUFFER_SIZE + 1)))
+        return (NULL);
+    line_len = contains_new_line(leftover);
+    if (line_len > 0)
+        return (extract_line(&leftover, line_len));
+    byte_read = 1;
+    while (line_len < 1 && byte_read > 0)
+    {
+        if ((byte_read = read(fd, buffer, BUFFER_SIZE)) < 0)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        leftover = ft_strjoin(leftover, buffer, byte_read);
+        line_len = contains_new_line(leftover);
+    }
+    free(buffer);
+    return (endl_checker(&leftover, line_len));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:06:11 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/22 22:29:29 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/25 19:58:11 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_strlen(const char	*str)
+int	ft_strlen(const char *str)
 {
 	int	i;
 
@@ -24,30 +24,76 @@ static int	ft_strlen(const char	*str)
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(const char *s1, const char *s2, int len2)
+{
+    char	*str;
+    size_t	i;
+    size_t	j;
+    int	len1;
+
+    len1 = ft_strlen(s1);
+    str = (char *)malloc(len1 + len2 + 1);
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (s1 && i < len1)
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    j = 0;
+    while (s2 && j < len2)
+    {
+        str[i] = s2[j];
+        i++;
+        j++;
+    }
+    str[i] = '\0';
+    free(s1);
+    return (str);
+}
+
+int	contains_new_line(const char *str)
+{
+	int i;
+	
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i] && str[i] != '\n')
+		i ++;
+	if (str[i] == '\n')
+		return (i + 1);
+	return (0);
+}
+
+char	*extract_line(char **leftover, int len)
 {
 	char	*str;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	char	*remainder;
+	int	i;
+	
+	if (len < 1 || !leftover || !*leftover)
+        return (NULL);
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	while (s1 && s1[i])
+	i = 0;
+	while ((*leftover)[i] && i < len)
 	{
-		str[i] = s1[i];
+		str[i] = (*leftover)[i];
 		i++;
-	}
-	while (s2 && s2[j])
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
 	}
 	str[i] = '\0';
-	free(s1);
-	free(s2);
+	remainder = ft_strjoin("", *leftover + i, ft_strlen(*leftover + i));
+	free(*leftover);
+	*leftover = remainder;
 	return (str);
+}
+
+char	*endl_checker(char **leftover, int line_len)
+{
+    if (line_len > 0)
+        return (extract_line(leftover, line_len));
+    return (*leftover);
 }
